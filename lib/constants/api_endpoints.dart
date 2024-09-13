4ff5be06c506595dc2ff3b1ff14906bbb5b15761
Nazmi Ceylan <66071359+NazmiCyln@users.noreply.github.com>
2024-09-13 09:50:39 +0300

    Update api_endpoints.dart

--- lib/constants/api_endpoints.dart
+++ lib/constants/api_endpoints.dart
@@ -1,9 +1,1 @@
-// ignore_for_file: avoid_classes_with_only_static_members
 
-abstract final class Endpoints {
-  static const baseUrl = "https://hardy-crane-66.hasura.app/api/rest/";
-
-  static const createUser = "createUser";
-
-  static const getStatusType = "getStatusType";
-}
